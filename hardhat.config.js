623576219f6997092909d28558f1109710b3e495
Ademu Anthony <ademuanthony@gmail.com>
2023-02-24 18:29:08 +0400

    contract

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat:{},
+    localhost: {
+      forking: {
+        url: "https://polygon-mainnet.g.alchemy.com/v2/"+process.env.ALCHEMY_KEY,
+        blockNumber: 29773646
+      }
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygon: {
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.PRIVATE_KEY],
+      // gasPrice: 36000000000
+    },
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    bsc: {
+      url: process.env.BSC_MAINNODE,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      bsc:  process.env.BSC_API_KEY,
+      bscTestnet: process.env.BSC_API_KEY
+    }
+  },
+  solidity: {
+    version: "0.8.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
