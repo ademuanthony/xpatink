bfde5fc57bf60c18df5ba175e574b21c27709264
Ademu Anthony <ademuanthony@gmail.com>
2023-02-27 02:41:59 +0400

    no limit

--- contracts/AntiSniper.sol
+++ contracts/AntiSniper.sol
@@ -7,71 +7,91 @@ import "./Interfaces.sol";
 import "./BaseErc20.sol";
 
 interface IPinkAntiBot {
-  function setTokenOwner(address owner) external;
-  function onPreTransferCheck(address from, address to, uint256 amount) external;
+    function setTokenOwner(address owner) external;
+
+    function onPreTransferCheck(
+        address from,
+        address to,
+        uint256 amount
+    ) external;
 }
 
 abstract contract AntiSniper is BaseErc20 {
-    
     IPinkAntiBot public pinkAntiBot;
-    bool private pinkAntiBotConfigured;
 
     bool public enableSniperBlocking;
     bool public enableBlockLogProtection;
     bool public enableHighTaxCountdown;
     bool public enablePinkAntiBot;
-    
+
     uint256 public msPercentage;
     uint256 public mhPercentage;
-    uint256 public maxGasLimit;
 
     uint256 public launchTime;
     uint256 public launchBlock;
     uint256 public snipersCaught;
-    
-    mapping (address => bool) public isSniper;
-    mapping (address => bool) public isNeverSniper;
-    mapping (address => uint256) public transactionBlockLog;
-    
+
+    mapping(address => bool) public isSniper;
+    mapping(address => bool) public isNeverSniper;
+    mapping(address => uint256) public transactionBlockLog;
+
     // Overrides
-    
+
     function configure(address _owner) internal virtual override {
         isNeverSniper[_owner] = true;
+        // BSC address from https://github.com/pinkmoonfinance/pink-antibot-guide
+        pinkAntiBot = IPinkAntiBot(0x8EFDb3b642eb2a20607ffe0A56CFefF6a95Df002);
         super.configure(_owner);
     }
-    
-    function launch() override virtual public onlyOwner {
+
+    function launch() public virtual override onlyOwner {
         super.launch();
         launchTime = block.timestamp;
         launchBlock = block.number;
     }
-    
-    function preTransfer(address from, address to, uint256 value) override virtual internal {
-        require(enableSniperBlocking == false || isSniper[msg.sender] == false, "sniper rejected");
-        
-        if (launched && from != owner && isNeverSniper[from] == false && isNeverSniper[to] == false) {
-            
-            if (maxGasLimit > 0) {
-               require(gasleft() <= maxGasLimit, "this is over the max gas limit");
-            }
-            
+
+    function preTransfer(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        require(
+            enableSniperBlocking == false || isSniper[msg.sender] == false,
+            "sniper rejected"
+        );
+
+        if (
+            launched &&
+            from != owner &&
+            isNeverSniper[from] == false &&
+            isNeverSniper[to] == false
+        ) {
             if (mhPercentage > 0 && exchanges[to] == false) {
-                require (_balances[to] + value <= mhAmount(), "this is over the max hold amount");
+                require(
+                    _balances[to] + value <= mhAmount(),
+                    "this is over the max hold amount"
+                );
             }
-            
+
             if (msPercentage > 0 && exchanges[to]) {
-                require (value <= msAmount(), "this is over the max sell amount");
+                require(
+                    value <= msAmount(),
+                    "this is over the max sell amount"
+                );
             }
-            
-            if(enableBlockLogProtection) {
-                if (transactionBlockLog[to] == block.number) {
-                    isSniper[to] = true;
-                    snipersCaught ++;
-                }
-                if (transactionBlockLog[from] == block.number) {
-                    isSniper[from] = true;
-                    snipersCaught ++;
-                }
+
+            if (enableBlockLogProtection) {
+                require(
+                    !(transactionBlockLog[from] >= block.number - 2 &&
+                        exchanges[to]),
+                    "potential frontrun attack"
+                );
+                require(
+                    !(transactionBlockLog[to] >= block.number - 2 &&
+                        exchanges[from]),
+                    "potential frontrun attack"
+                );
+
                 if (exchanges[to] == false) {
                     transactionBlockLog[to] = block.number;
                 }
@@ -79,90 +99,80 @@ abstract contract AntiSniper is BaseErc20 {
                     transactionBlockLog[from] = block.number;
                 }
             }
-            
+
             if (enablePinkAntiBot) {
                 pinkAntiBot.onPreTransferCheck(from, to, value);
             }
         }
-        
+
         super.preTransfer(from, to, value);
     }
-    
-    function calculateTransferAmount(address from, address to, uint256 value) internal virtual override returns (uint256) {
+
+    function calculateTransferAmount(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override returns (uint256) {
         uint256 amountAfterTax = value;
         if (launched && enableHighTaxCountdown) {
-            if (from != owner && sniperTax() > 0 && isNeverSniper[from] == false && isNeverSniper[to] == false) {
+            if (
+                from != owner &&
+                sniperTax() > 0 &&
+                isNeverSniper[from] == false &&
+                isNeverSniper[to] == false
+            ) {
                 uint256 taxAmount = (value * sniperTax()) / 10000;
                 amountAfterTax = amountAfterTax - taxAmount;
             }
         }
         return super.calculateTransferAmount(from, to, amountAfterTax);
     }
-    
+
     // Public methods
-    
+
     function mhAmount() public view returns (uint256) {
         return (_totalSupply * mhPercentage) / 10000;
     }
-    
+
     function msAmount() public view returns (uint256) {
-         return (_totalSupply * msPercentage) / 10000;
+        return (_totalSupply * msPercentage) / 10000;
     }
-    
-   function sniperTax() public virtual view returns (uint256) {
-        if(launched) {
+
+    function sniperTax() public view virtual returns (uint256) {
+        if (launched) {
             if (block.number - launchBlock < 3) {
                 return 9900;
             }
         }
         return 0;
     }
-    
-    // Admin methods
-    
-    function configurePinkAntiBot(address antiBot) external onlyOwner {
-        pinkAntiBot = IPinkAntiBot(antiBot);
-        pinkAntiBot.setTokenOwner(owner);
-        pinkAntiBotConfigured = true;
-        enablePinkAntiBot = true;
-    }
-    
+
     function setSniperBlocking(bool enabled) external onlyOwner {
         enableSniperBlocking = enabled;
     }
-    
+
     function setBlockLogProtection(bool enabled) external onlyOwner {
         enableBlockLogProtection = enabled;
     }
-    
+
     function setHighTaxCountdown(bool enabled) external onlyOwner {
         enableHighTaxCountdown = enabled;
     }
-    
+
     function setPinkAntiBot(bool enabled) external onlyOwner {
-        require(pinkAntiBotConfigured, "pink anti bot is not configured");
         enablePinkAntiBot = enabled;
+        if (enabled) {
+            pinkAntiBot.setTokenOwner(owner);
+        }
     }
-    
+
     function setMsPercentage(uint256 amount) external onlyOwner {
+        require(amount >= 300, "Max hold cannot be less than 3%");
         msPercentage = amount;
     }
-    
+
     function setMhPercentage(uint256 amount) external onlyOwner {
+        require(amount >= 300, "Max hold cannot be less than 3%");
         mhPercentage = amount;
     }
-    
-    function setMaxGasLimit(uint256 amount) external onlyOwner {
-        maxGasLimit = amount;
-    }
-    
-    function setIsSniper(address who, bool enabled) external onlyOwner {
-        isSniper[who] = enabled;
-    }
-
-    function setNeverSniper(address who, bool enabled) external onlyOwner {
-        isNeverSniper[who] = enabled;
-    }
-
-    // private methods
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,50 +1,49 @@
 require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
-// require('@openzeppelin/hardhat-upgrades');
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
-    hardhat:{},
+    hardhat: {},
     localhost: {
       forking: {
-        url: "https://polygon-mainnet.g.alchemy.com/v2/"+process.env.ALCHEMY_KEY,
-        blockNumber: 29773646
-      }
+        url:
+          'https://polygon-mainnet.g.alchemy.com/v2/' + process.env.ALCHEMY_KEY,
+        blockNumber: 29773646,
+      },
     },
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.PRIVATE_KEY],
     },
     polygon: {
-      url: "https://polygon-rpc.com/",
+      url: 'https://polygon-rpc.com/',
       accounts: [process.env.PRIVATE_KEY],
-      // gasPrice: 36000000000
     },
     bscTestnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-      accounts: [process.env.PRIVATE_KEY]
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      accounts: [process.env.PRIVATE_KEY],
     },
     mainnet: {
-      url: "https://mainnet.infura.io/v3/",
-      accounts: [process.env.PRIVATE_KEY]
-    }
+      url: process.env.INFURA_MAINNET,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
     apiKey: {
       polygon: process.env.POLYGONSCAN_API_KEY,
-      bsc:  process.env.BSC_API_KEY,
-      bscTestnet: process.env.BSC_API_KEY
-    }
+      bsc: process.env.BSC_API_KEY,
+      bscTestnet: process.env.BSC_API_KEY,
+    },
   },
   solidity: {
-    version: "0.8.7",
+    version: '0.8.7',
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
-}
+};
